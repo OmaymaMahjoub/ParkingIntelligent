14334154b23a5b06f7e6b98d4c31a9b5f45c47ab
moenesbs <63344830+moenesbs@users.noreply.github.com>
2020-09-06 18:14:09 +0200

    Add files via upload

--- Interface.py
+++ Interface.py
@@ -1,39 +1,43 @@
 from tkinter import * 
-
+from Emplacement import *
 homePage = Tk()
 homePage['bg']='gray'
+    
 
-#Etage Name
-label = Label(homePage, text="E1", bg="gray", fg="SlateBlue4")
-label.config(font=("Roman bold", 30))
-label.grid(row=0, column=3)
+#Menu principal
+def Home():
+    label = Label(homePage, text="Bienvenue", bg="gray", fg="SlateBlue4" )
+    label.config(font=("Roman bold", 30))
+    label.place(x=150, y=0)
+    reservation=Button(homePage,text ="Gestion de réservartion", bg='red' )
+    reservation.place(x=10, y=100)
+    reservation.config(command = gest_reservation )
+    surplace=Button(homePage,text ="Parking direct", bg='red', )
+    surplace.place(x=350, y=100)
+    homePage.mainloop()
 
-#cars
-reserve1 = PhotoImage(file = r".\images\BCarDown.png")
-reserve2 = PhotoImage(file = r".\images\BCarUp.png")
-libre1= PhotoImage(file = r".\images\GCarDown.png")
-libre2= PhotoImage(file = r".\images\GCarUp.png")
-occupe1= PhotoImage(file = r".\images\RCarDown.png")
-occupe2= PhotoImage(file = r".\images\RCarUp.png")
+#Gestion de reservartion
+def gest_reservation():
+    res= Tk()
+    res['bg']='gray'
+    labelr = Label(res, text="Gestion de réservartion", bg="gray", fg="SlateBlue4" )
+    labelr.config(font=("Roman bold", 30))
+    labelr.place(x=150, y=0)
+    print("test")
+    reserverB = Button(res,text="Reserver", bg="red")
+    reserverB.place(x=10, y=100)
+    reserverB.config(command = reserver() )
+    valider = Button(res,text="Valider", bg='red')
+    valider.place(x=350,y=100)
+    
+def reserver():
+    print('testee')
 
-#Car button exeample row1
-button1=Button(homePage, text = "Button", image = reserve1, bg='gray')
-button1.grid(row=1,column=0)
-button2=Button(homePage, text = "Button", image = reserve1, bg='gray')
-button2.grid(row=1,column=1)
-button3=Button(homePage, text = "Button", image = libre1, bg='gray')
-button3.grid(row=1,column=2)
-button4=Button(homePage, text = "Button", image = occupe1, bg='gray')
-button4.grid(row=1,column=3)
 
-#Car button exeample row1
-button11=Button(homePage, text = "Button", image = reserve2, bg='gray')
-button11.grid(row=3,column=0)
-button22=Button(homePage, text = "Button", image = reserve2, bg='gray')
-button22.grid(row=3,column=1)
-button33=Button(homePage, text = "Button", image = libre2, bg='gray')
-button33.grid(row=3,column=2)
-button44=Button(homePage, text = "Button", image = occupe2, bg='gray')
-button44.grid(row=3,column=3)
+Home()
 
-homePage.mainloop()
+
+        
+
+
+
